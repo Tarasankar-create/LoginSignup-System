b282ba47dbd608a3244b9df9ebbbc2891c910975
Tarasankar-create <dastarasankar393@gmail.com>
2025-01-17 19:29:09 +0530

    Update admin login

--- assessment1/urls.py
+++ assessment1/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from assessment1.views import login,signup
+from assessment1.views import login,signup,userhome,adminhome
 
 urlpatterns = [
     path('',login,name='login'),
     path('signup',signup,name='signup'),
+    path('userhome',userhome,name='userhome'),
+    path('adminhome',adminhome,name='adminhome'),
 ]
--- assessment1/views.py
+++ assessment1/views.py
@@ -2,7 +2,28 @@ from django.shortcuts import render,redirect
 from .models import user_master
 
 # Create your views here.
+def userhome(request):
+    name=request.session.get('username')
+    ob=user_master.objects.get(user_name=name)
+    return render(request,'userhome.html',{'Username':name,'data':ob})
+def adminhome(request):
+    name=request.session.get('username')
+    obj=user_master.objects.get(user_name=name)
+    ob=user_master.objects.filter(roll_name='user')
+    return render(request,'adminhome.html',{'Username':name,'data':obj,'user':ob})
 def login(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        pwd=request.POST['pwd']
+        try:
+            ob=user_master.objects.get(user_name=username,password=pwd)
+            request.session['username']=ob.user_name
+            if ob.roll_name=='user':
+                return redirect('userhome')
+            elif ob.roll_name=='admin':
+                return redirect('adminhome')
+        except Exception as e:
+            return render(request,'login.html',{'msg':"Invalid user or password " +str(e)})
     return render(request,'login.html')
 def signup(request):
     if request.method=="POST":
